8765dd0d9bf49faedde9dd6cdb04ae7c8af601cc
Viacheslav Oblog <slavaoblog@ya.ru>
2024-01-10 17:32:58 +0200

    changed ddl-auto to update

--- src/main/java/com/example/matchmadness/component/DataInitializer.java
+++ src/main/java/com/example/matchmadness/component/DataInitializer.java
@@ -6,12 +6,14 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Component
+@Profile("development")
 @AllArgsConstructor
 public class DataInitializer implements ApplicationRunner {
 
@@ -21,17 +23,17 @@ public class DataInitializer implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         List<Word> wordList = Arrays.asList(
-//                new Word("Haus", "house"),
-//                new Word("Licht", "light"),
-//                new Word("Witz", "joke"),
-//                new Word("Mann", "man"),
-//                new Word("Frau", "woman"),
-//                new Word("schnell", "quick"),
-//                new Word("groß", "large"),
-//                new Word("klein", "little"),
-//                new Word("Käse", "cheese"),
-//                new Word("Beruf", "profession"),
-//                new Word("Teil", "part"),
+                new Word("Haus", "house"),
+                new Word("Licht", "light"),
+                new Word("Witz", "joke"),
+                new Word("Mann", "man"),
+                new Word("Frau", "woman"),
+                new Word("schnell", "quick"),
+                new Word("groß", "large"),
+                new Word("klein", "little"),
+                new Word("Käse", "cheese"),
+                new Word("Beruf", "profession"),
+                new Word("Teil", "part"),
                 new Word("Kind", "kid")
         );
 
